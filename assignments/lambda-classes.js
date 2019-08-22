cd332ceaa419e0d66eb25655dac4b27f70e85d67
ericbanker12 <ericbanker12@gmail.com>
2019-08-22 15:05:23 -0500

    complete classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,4 +1,6 @@
 // CODE here for your Lambda Classes
+
+// Person
 class Person {
     constructor(obj) {
         for (let key of [`name`, `age`, `location`]) {
@@ -10,9 +12,13 @@ class Person {
 
 const testPersonOne = new Person({name: 'Mr. Test 1', age: 999, location: 'Earth'})
 const testPersonTwo = new Person({name: 'Mr. Test 2', age: 555, location: 'Mars'})
+
+console.log('\nPerson 1') // spacing sections
 testPersonOne.speak()
+console.log('\nPerson 2') // spacing sections
 testPersonTwo.speak()
 
+// Instructor
 class Instructor extends Person {
     constructor(obj) {
         super(obj)
@@ -26,11 +32,15 @@ class Instructor extends Person {
 
 const testInstructorOne = new Instructor({name: 'Professor Test 1', age: 999, location: 'Jupiter', specialty: 'Math', favLanguage: 'Javascript', catchPhrase: 'Hello World!'})
 const testInstructorTwo = new Instructor({name: 'Professor Test 2', age: 555, location: 'Pluto', specialty: 'Science', favLanguage: 'C', catchPhrase: 'Hello World!'})
+
+console.log('\nInstructor 1') // spacing sections
 testInstructorOne.speak()
 testInstructorOne.demo('History')
+console.log('\nInstructor 2') // spacing sections
 testInstructorTwo.speak()
 testInstructorTwo.demo('Art')
 
+// Student
 class Student extends Person {
     constructor(obj) {
         super(obj)
@@ -39,19 +49,67 @@ class Student extends Person {
         }
     }
     listsSubjects() {this.favSubjects.forEach(e=>{console.log(e)})} //console.log(this.favSubjects.join('\n'))
-    PRAssignment(subject) {console.log(`student.name has submitted a PR for ${subject}`)}
-    sprintChallenge(subject) {console.log(`student.name has begun sprint challenge on ${subject}`)}
+    PRAssignment(subject) {console.log(`${this.name} has submitted a PR for ${subject}`)}
+    sprintChallenge(subject) {console.log(`${this.name} has begun sprint challenge on ${subject}`)}
 }
 
 const testStudentOne = new Student({name: 'Test 1', age: 999, location: 'Neptune', previousBackground: 'Math', className: 'Freshman', favSubjects: ['Math', 'Science']})
 const testStudentTwo = new Student({name: 'Test 2', age: 555, location: 'Uranus', previousBackground: 'Science', className: 'Senior', favSubjects: ['P.E.']})
+
+console.log('\nStudent 1') // spacing sections
 testStudentOne.speak()
 testStudentOne.listsSubjects()
 testStudentOne.PRAssignment('Math')
 testStudentOne.sprintChallenge('Science')
 testInstructorOne.grade(testStudentOne, 'Science')
+console.log('\nStudent 2') // spacing sections
 testStudentTwo.speak()
 testStudentTwo.listsSubjects()
 testStudentTwo.PRAssignment('Art')
 testStudentTwo.sprintChallenge('English')
 testInstructorTwo.grade(testStudentTwo, 'English')
+
+// Project Manager
+class ProjectManagers extends Instructor {
+    constructor(obj) {
+        super(obj)
+        for (let key of [`gradClassName`, `favInstructor`]) {
+            this[key] = obj[key]
+        }
+    }
+    standUp(channel) {console.log(`${this.name} announces to ${channel}, @channel standy times!​​​​​`)}
+    debugsCode(student, subject) {console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)}
+}
+
+const testProjectManagersOne = new ProjectManagers({
+    name: 'PM Test 1', 
+    age: 999, 
+    location: 'Mercury', 
+    specialty: 'Math', 
+    favLanguage: 'Javascript', 
+    catchPhrase: 'Hello World!', 
+    gradClassName: 'web20', 
+    favInstructor: 'Ty'
+})
+const testProjectManagersTwo = new ProjectManagers({
+    name: 'PM Test 2', 
+    age: 555, 
+    location: 'the Moon', 
+    specialty: 'Science', 
+    favLanguage: 'C', 
+    catchPhrase: 'Hello World!', 
+    gradClassName: 'web20', 
+    favInstructor: 'Ty'
+})
+console.log('\nProject Manager 1') // spacing sections
+testProjectManagersOne.speak()
+testProjectManagersOne.demo('History')
+testProjectManagersOne.standUp('#web23_PM_Test_1')
+testProjectManagersOne.debugsCode(testStudentOne, 'Science')
+testProjectManagersOne.grade(testStudentOne, 'Science')
+console.log('\nProject Manager 2') // spacing sections
+testProjectManagersTwo.speak()
+testProjectManagersTwo.demo('Art')
+testProjectManagersTwo.standUp('#web23_PM_Test_2')
+testProjectManagersTwo.debugsCode(testStudentTwo, 'Math')
+testProjectManagersTwo.grade(testStudentTwo, 'Math')
